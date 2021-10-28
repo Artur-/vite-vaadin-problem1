caccc24c1a4dafaf1803c64e2882f6a6aa63929c
Artur Signell <artur@vaadin.com>
2021-10-28 14:40:33 +0300

    Simplify

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,7 +1,12 @@
 import '@polymer/iron-list/iron-list.js';
-import { Debouncer } from "@polymer/polymer/lib/utils/debounce.js";
-console.log(Debouncer);
+import "@polymer/polymer/lib/utils/debounce.js";
 import '@vaadin/vaadin-grid-pro/theme/lumo/vaadin-grid-pro-edit-column.js';
-import '@vaadin/vaadin-grid/theme/lumo/vaadin-grid.js';
 import '@vaadin/vaadin-lumo-styles/color.js';
-import '@vaadin/vaadin-virtual-list/vaadin-virtual-list.js';
+import '@vaadin/vaadin-lumo-styles/font-icons.js';
+import '@vaadin/vaadin-lumo-styles/sizing.js';
+import '@vaadin/vaadin-lumo-styles/spacing.js';
+import '@vaadin/vaadin-lumo-styles/style.js';
+import '@vaadin/vaadin-lumo-styles/typography.js';
+import '@vaadin/vaadin-checkbox/theme/lumo/vaadin-checkbox.js';
+import '@polymer/polymer/lib/elements/dom-module.js';
+
