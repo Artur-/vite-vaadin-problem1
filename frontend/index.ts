e61111084f7f3f60f090f11b4b01602c869ea80d
Artur Signell <artur@vaadin.com>
2021-10-28 14:46:57 +0300

    Simplify

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,12 +1,4 @@
 import '@polymer/iron-list/iron-list.js';
 import "@polymer/polymer/lib/utils/debounce.js";
-import '@vaadin/vaadin-grid-pro/theme/lumo/vaadin-grid-pro-edit-column.js';
-import '@vaadin/vaadin-lumo-styles/color.js';
-import '@vaadin/vaadin-lumo-styles/font-icons.js';
-import '@vaadin/vaadin-lumo-styles/sizing.js';
-import '@vaadin/vaadin-lumo-styles/spacing.js';
-import '@vaadin/vaadin-lumo-styles/style.js';
-import '@vaadin/vaadin-lumo-styles/typography.js';
-import '@vaadin/vaadin-checkbox/theme/lumo/vaadin-checkbox.js';
 import '@polymer/polymer/lib/elements/dom-module.js';
-
+import '@vaadin/vaadin-select/theme/lumo/vaadin-select.js';
